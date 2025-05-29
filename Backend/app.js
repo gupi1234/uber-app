d90454de693e0c782c3c614b3d46f5634fb78fe4
gupi debnath <iamgupidebntah@gmail.com>
2025-05-29 19:25:16 +0530

    user registation done

--- Backend/app.js
+++ Backend/app.js
@@ -3,9 +3,19 @@ dotenv.config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const connectToDo = require("./db/db");
+const userRoutes = require("./routes/user.routes");
+
+connectToDo();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("hello");
 });
+
+app.use("/users", userRoutes);
+
+module.exports = app;
---
+++ Backend/services/user.service.js
@@ -0,0 +1,23 @@
+const userModel = require("../models/user.model");
+
+module.exports.createUser = async ({
+  firstname,
+  lastname,
+  email,
+  password,
+}) => {
+  if (!firstname || !email || !password) {
+    throw new Error("All fields are required");
+  }
+
+  const user = userModel({
+    fullname: {
+      firstname: firstname,
+      lastname: lastname,
+    },
+    email,
+    password,
+  });
+
+  return user;
+};
